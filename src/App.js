ef246f68da23d464fe3ec4d9bc6bc95657c7c8bf
softlee144 <ockjungbong@gmail.com>
2024-07-23 22:00:44 +0900

    최초 등록

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+// 0. React - 데이터 변경감지해서 UI 그려주는 엔진
+// 1. 실행방식 - SPA
+// 2. jsx 문법
+// 3. 바벨(ES5) -> ES6
 function App() {
+  let number = 1;
+
+  const add = () => {
+    number++;
+    console.log('add', add);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>숫자:{number}</h1>
+      <button onClick={add}>더하기</button>
     </div>
   );
 }
